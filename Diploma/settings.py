8463996dd6de60450dcc52e80fb20a1134e4ea58
danil <dhushchyn>
2022-07-27 17:49:40 +0300

    SECOND COMMIT

--- Diploma/settings.py
+++ Diploma/settings.py
@@ -84,7 +84,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'kino_site',
-        'USER': 'postgres',
+        'USER': 'danil',
         'PASSWORD': 'buzaho4114',
         'HOST': '127.0.0.1',
         'PORT': 5432
